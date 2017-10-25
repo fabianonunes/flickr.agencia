baa10e5ed67f394c287212bbf67a778a54a44212
fabianonunes <fabiano@nunes.me>
2017-10-25 14:48:00 -0200

    aula 3

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,19 @@ module.exports = {
   },
   plugins: [
     new Extractor('[name].css')
+  ],  module: {
+    rules: [{
+      test: /\.pug$/, loader: 'pug-loader'
+    }, {
+      test: /\.less$/,
+      loader: Extractor.extract(['css-loader', 'less-loader'])
+    }, {
+      test: /\.(svg|woff|ttf|eot|woff2)$/,
+      loaders: ['file-loader?name=fonts/[name].[hash:base64:5].[ext]']
+    }]
+  },
+  plugins: [
+    new Extractor('[name].css')
   ],
   devServer: {
     contentBase: path.resolve('./app'),
