5286392fff7cebb330da17ad32323725f0280adc
fabianonunes <fabiano@nunes.me>
2017-10-26 14:35:24 -0200

    cegn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,22 +6,16 @@ var templatePugPhoto = require('./templates/photo.pug')
 var api = new FlickrApi('026d26f0c2e252ec152c416857ecd75c')
 var $loading = $('.Loading')
 
-$('#input').on('keyup', function (evt) {
-  if (evt.keyCode === 13) {
-    $loading.removeClass('is-hidden')
-    busca($('#input').val()).then(function () {
-      $loading.addClass('is-hidden')
-    })
-  }
+$('#formBusca').on('submit', function (evt) {
+  $loading.removeClass('is-hidden')
+  busca($('#input').val()).then(function () {
+    $loading.addClass('is-hidden')
+  })
 })
 
 var busca = function (texto) {
   return api.search(texto)
   .then(function (photos) {
-    console.log(photos[0])
-    console.log(
-      templatePugPhoto(photos[0])
-    )
     var html = photos.map(templatePugPhoto)
     $('#photos').html(html.join(''))
   })
