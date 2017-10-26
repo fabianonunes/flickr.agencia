26233f2b352a55011e71453e9ed395b46cdec0d0
fabianonunes <fabiano@nunes.me>
2017-10-26 15:44:40 -0200

    cegn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,10 @@
-// var $ = require('jquery')
-// var FlickrApi = require('./lib/flickr-api')
+import preact from 'preact'
 
-// var templatePugPhoto = require('./templates/photo.pug')
+var App = function () {
+  return <div>Olá!</div>
+}
 
-// var api = new FlickrApi('026d26f0c2e252ec152c416857ecd75c')
-// var $loading = $('.Loading')
-
-// $('#formBusca').on('submit', function (evt) {
-//   $loading.removeClass('is-hidden')
-//   busca($('#input').val()).then(function () {
-//     $loading.addClass('is-hidden')
-//   })
-// })
-
-// var busca = function (texto) {
-//   return api.search(texto)
-//   .then(function (photos) {
-//     var html = photos.map(templatePugPhoto)
-//     $('#photos').html(html.join(''))
-//   })
-// }
+preact.render(
+  <App />,
+  document.getElementById('app')
+)
